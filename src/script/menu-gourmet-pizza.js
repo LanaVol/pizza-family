5d93d27f9384de06c60f4802ca4621d5c6dfe360
LanaVol <svolosh4yk@gmail.com>
2023-11-30 22:35:35 +0200

    created js for generation gourment-block(menu) items in mockup

---
+++ src/script/menu-gourmet-pizza.js
@@ -0,0 +1,19 @@
+import { gourmetPizza } from "./data";
+
+const menuGourmetBlock = document.querySelector(".gourmet");
+const list = [];
+
+gourmetPizza.map((item) => {
+  const menuGourmetItem = `<li class="block-item">
+                              <div>
+                                <p class="item-name">${item["name"]}</p>
+                                <p class="item-info">
+                                  ${item["ingredients"]}
+                                </p>
+                              </div>
+                              <p class="item-price">$${item["price"]}</p>
+                            </li>`;
+
+  list.push(menuGourmetItem);
+});
+menuGourmetBlock.insertAdjacentHTML("afterbegin", list.join(""));

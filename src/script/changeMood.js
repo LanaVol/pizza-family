5205fae503dd0ce0c4ecff6be87787c6572ffa02
LanaVol <svolosh4yk@gmail.com>
2023-12-16 23:52:18 +0200

    improved code with separated fs

--- src/script/changeMood.js
+++ src/script/changeMood.js
@@ -12,37 +12,36 @@ changeThemeBtn.addEventListener("click", changeColorTheme);
 // change dark/light background
 function changeColorTheme(e) {
   if (main.classList.contains("dark")) {
-    main.classList.remove("dark");
-    main.classList.add("light");
-    header.classList.remove("dark");
-    header.classList.add("light");
-    changeThemeBtn.setAttribute("src", themeBtnMoon);
-    // changeThemeBtn.setAttribute("src", "./img/01themeBtnMoon.svg");
-    changeMenuFontsColor(e);
-    changeBlockStyle(e);
+    toggleColorElements("dark", "light", themeBtnMoon);
   } else if (main.classList.contains("light")) {
-    main.classList.remove("light");
-    main.classList.add("dark");
-    header.classList.remove("light");
-    header.classList.add("dark");
-    changeThemeBtn.setAttribute("src", themeBtn);
-    // changeThemeBtn.setAttribute("src", "./img/01themeBtn.svg");
-    changeMenuFontsColor(e);
-    changeBlockStyle(e);
+    toggleColorElements("light", "dark", themeBtn);
   }
+  changeMenuFontsColor(e);
+  changeBlockStyle(e);
+}
+
+function toggleColorElements(removeClass, addClass, themeIcon) {
+  main.classList.remove(removeClass);
+  main.classList.add(addClass);
+  header.classList.remove(removeClass);
+  header.classList.add(addClass);
+  changeThemeBtn.setAttribute("src", themeIcon);
 }
 
 // change fonts color menu of changing theme
 function changeMenuFontsColor(e) {
   Array.from(menuBlocks).forEach((el) => {
     if (el.classList.contains("beige")) {
-      el.classList.remove("beige");
-      el.classList.add("brown");
+      toogleClass(el, "beige", "brown");
     } else if (el.classList.contains("brown")) {
-      el.classList.remove("brown");
-      el.classList.add("beige");
+      toogleClass(el, "brown", "beige");
     }
   });
+}
+
+function toogleClass(el, removeClass, addClass) {
+  el.classList.remove(removeClass);
+  el.classList.add(addClass);
 }
 
 // change style of wrapper blocks and form
@@ -51,22 +50,19 @@ function changeBlockStyle(e) {
     if (el.classList.contains("dark-gray")) {
       el.classList.remove("dark-gray");
       el.style.backgroundColor = "rgba(217, 217, 217, 0.59)";
-      el.classList.remove("white-fonts");
-      el.classList.add("black-fonts");
+      toogleClass(el, "white-fonts", "black-fonts");
     } else {
       el.style.backgroundColor = "";
       el.classList.add("dark-gray");
-      el.classList.remove("black-fonts");
-      el.classList.add("white-fonts");
+      toogleClass(el, "black-fonts", "white-fonts");
     }
   });
-  if (form.parentElement.classList.contains("white-fonts")) {
-    form.parentElement.classList.remove("white-fonts");
-    form.parentElement.classList.add("black-fonts");
-    form.parentElement.style.backgroundColor = "rgba(217, 217, 217, 0.59)";
-  } else if (form.parentElement.classList.contains("black-fonts")) {
-    form.parentElement.classList.remove("black-fonts");
-    form.parentElement.classList.add("white-fonts");
-    form.parentElement.style.backgroundColor = "rgba(255, 255, 255, 0.05)";
+  const formParent = form.parentElement;
+  if (formParent.classList.contains("white-fonts")) {
+    toogleClass(formParent, "white-fonts", "black-fonts");
+    formParent.style.backgroundColor = "rgba(217, 217, 217, 0.59)";
+  } else if (formParent.classList.contains("black-fonts")) {
+    toogleClass(formParent, "black-fonts", "white-fonts");
+    formParent.style.backgroundColor = "rgba(255, 255, 255, 0.05)";
   }
 }

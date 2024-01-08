7863c86702eade6e01d098205a51c4fd7830d1a1
LanaVol <svolosh4yk@gmail.com>
2024-01-08 20:42:58 +0200

    add validation to class DOM helper

--- src/script/DOMHelper.js
+++ src/script/DOMHelper.js
@@ -3,15 +3,44 @@ export class DOMHelper {
     return document.querySelector(selector);
   }
 
+  static #validateDOMElement(element) {
+    if (element instanceof Element) {
+      return true;
+    }
+    return false;
+  }
+
   static addClass(element, className) {
+    if (!DOMHelper.#validateDOMElement(element)) return;
+
     element.classList.add(className);
   }
 
   static removeClass(element, className) {
+    if (!DOMHelper.#validateDOMElement(element)) return;
+
     element.classList.remove(className);
   }
 
   static setTextContent(element, text) {
+    if (!DOMHelper.#validateDOMElement(element)) return;
+
     element.textContent = text;
   }
+
+  static toggleClass(element, removeClass, addClass) {
+    if (!DOMHelper.#validateDOMElement(element)) return;
+
+    DOMHelper.removeClass(element, removeClass);
+    DOMHelper.addClass(element, addClass);
+  }
+
+  // static isContainsClass(element, className) {
+  //   if (!DOMHelper.#validateDOMElement(element)) return;
+
+  //   if (element.classList.contains(className)) {
+  //     return true;
+  //   }
+  //   return false;
+  // }
 }

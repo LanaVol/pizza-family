f037af6dbf3307184dbf8cb7ad12630c16549231
LanaVol <svolosh4yk@gmail.com>
2024-01-13 01:45:24 +0200

    add drop f for elems

--- src/script/Pizza.js
+++ src/script/Pizza.js
@@ -9,6 +9,18 @@ export class Pizza {
     this.discount = 0;
     this.ownOrder = [];
     this.prevPizza = DOMHelper.select(".prevImg");
+    this.draghandle();
+  }
+
+  draghandle() {
+    this.prevPizza.addEventListener("dragover", (e) => this.preventEvent(e));
+    this.prevPizza.addEventListener("drop", (e) =>
+      this.dropElementToPreview(e)
+    );
+  }
+
+  preventEvent(e) {
+    e.preventDefault();
   }
 
   chooseSauseToPizzaPreview(img) {
@@ -20,5 +32,22 @@ export class Pizza {
     DOMHelper.addClass(inscription, "inscription");
     this.prevPizza.appendChild(inscription);
     inscription.style.backgroundImage = `url(${url})`;
+  }
+
+  dropElementToPreview(e) {
+    this.preventEvent(e);
+
+    this.ownOrder
+      .sort((a, b) => a.data - b.data)
+      .forEach((el) => {
+        switch (el.category) {
+          case "cake":
+            return (prevSize.innerHTML = el.name);
+          case "sauce":
+            return this.chooseSauseToPizzaPreview(el.prevImg);
+          case "ingredient":
+            return this.createNewLayerPizza(el.withIngredient);
+        }
+      });
   }
 }

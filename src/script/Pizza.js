d5ad75836ca4a27d46759a80ff05db03f0efd675
LanaVol <svolosh4yk@gmail.com>
2024-01-14 22:59:47 +0200

    add drad&drop fs of ingredients & updated

--- src/script/Pizza.js
+++ src/script/Pizza.js
@@ -1,5 +1,7 @@
 import { DOMHelper } from "./DOMHelper";
 import { UserFormData } from "./UserFormData";
+import { cakesList, ingredientsList, saucesList } from "./data";
+import { showModalWindowError } from "./modals";
 
 export class Pizza {
   constructor() {
@@ -9,14 +11,37 @@ export class Pizza {
     this.discount = 0;
     this.ownOrder = [];
     this.prevPizza = DOMHelper.select(".prevImg");
-    this.draghandle();
+    this.constructorBlockMenu = DOMHelper.select(".constructor");
+    this.previewSize = DOMHelper.select(".previewSize");
+    this.dragOverHandle();
+    this.dropHandle();
+    this.dragStartHandle();
   }
 
-  draghandle() {
+  dragOverHandle() {
     this.prevPizza.addEventListener("dragover", (e) => this.preventEvent(e));
+  }
+
+  dropHandle() {
     this.prevPizza.addEventListener("drop", (e) =>
       this.dropElementToPreview(e)
     );
+  }
+
+  dragStartHandle() {
+    this.constructorBlockMenu.addEventListener("dragstart", (e) => {
+      switch (e.target.getAttribute("data-category")) {
+        case "cake":
+          this.getChosenCake(e);
+          break;
+        case "sauce":
+          this.addIngredientsToPizza(e, saucesList);
+          break;
+        case "ingredient":
+          this.addIngredientsToPizza(e, ingredientsList);
+          break;
+      }
+    });
   }
 
   preventEvent(e) {
@@ -36,18 +61,54 @@ export class Pizza {
 
   dropElementToPreview(e) {
     this.preventEvent(e);
+    console.log("2", this.ownOrder);
 
     this.ownOrder
       .sort((a, b) => a.data - b.data)
       .forEach((el) => {
         switch (el.category) {
           case "cake":
-            return (prevSize.innerHTML = el.name);
+            return (this.previewSize.innerHTML = el.name);
           case "sauce":
             return this.chooseSauseToPizzaPreview(el.prevImg);
           case "ingredient":
             return this.createNewLayerPizza(el.withIngredient);
         }
       });
+  }
+
+  getChosenCake(e) {
+    cakesList.forEach((oneCake) => {
+      if (e.target.getAttribute("data-name") === oneCake.name) {
+        if (this.ownOrder.length === 0) {
+          this.ownOrder.push({ ...oneCake, quantity: 1 });
+        } else if (this.ownOrder.length !== 0) {
+          const item = this.ownOrder.find((obj) => obj.name === oneCake.name);
+          if (!item) {
+            this.ownOrder.splice(0, 1, { ...oneCake, quantity: 1 });
+          } else {
+            return;
+          }
+        }
+      }
+    });
+  }
+
+  addIngredientsToPizza(e, ingredientsList) {
+    ingredientsList.forEach((element) => {
+      if (e.target.getAttribute("data-name") === element.name) {
+        if (this.ownOrder.length === 0) {
+          showModalWindowError("First choose the size:)");
+        } else if (this.ownOrder.length !== 0) {
+          const i = this.ownOrder.findIndex((obj) => obj.name === element.name);
+          if (i !== -1) {
+            this.ownOrder[i].quantity++;
+            this.ownOrder[i].data = new Date();
+          } else {
+            this.ownOrder.push({ ...element, quantity: 1, data: new Date() });
+          }
+        }
+      }
+    });
   }
 }

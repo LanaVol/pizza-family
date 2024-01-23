a2a30a16e9f9a85343857e403a8505b11d7e3641
LanaVol <svolosh4yk@gmail.com>
2024-01-23 20:53:41 +0200

    add methods for work with order & display to bill & update

--- src/script/PizzaOrder.js
+++ src/script/PizzaOrder.js
@@ -5,18 +5,44 @@ export class PizzaOrder {
   constructor() {
     this.orderSum = 0;
     this.orders = [];
-    this.totalPrice = 0;
     this.discount = 0;
+    this.totalPrice = 0;
+    this.outputInfoBill = DOMHelper.select(".order-list");
     this.totalPriceFieldInBill = DOMHelper.select("#total-price");
     this.discountInBill = DOMHelper.select("#discount");
     this.toPayFieldBill = DOMHelper.select("#totalPay");
+    this.cleanAllInBill = DOMHelper.select(".output-clean");
+    this.cleanAllBillInfo();
   }
 
   addPizzaToOrder(pizza) {
     this.orders.push(pizza);
   }
 
-  showInfoAboutOrderInBill() {}
+  removeOrderListBill() {
+    if (this.orders.length > 1) {
+      DOMHelper.removeListEls(this.outputInfoBill.children);
+    }
+  }
+
+  // need to fix after create basket
+  displayOrderListBill() {
+    this.removeOrderListBill();
+
+    this.orders.forEach((els) => {
+      let li = DOMHelper.createElement("li");
+      els.forEach((el) => {
+        if (
+          el.category === "cake" ||
+          el.category === "sauce" ||
+          el.category === "ingredient"
+        ) {
+          li.innerHTML += ` ${el.name}, `;
+        }
+      });
+      DOMHelper.appendChild(this.outputInfoBill, li);
+    });
+  }
 
   calcResultToPayWithDiscount() {
     let orderSum = 0;
@@ -34,15 +60,45 @@ export class PizzaOrder {
       this.orderSum = orderSum;
     });
 
-    this.showInfoAboutOrderInBill();
-    this.totalPriceFieldInBill.value = this.orderSum; //order-sum without discont and show at Item Total(bill)
-    this.discountInBill.value = this.discount * 100;
+    this.displayOrderListBill();
+
     this.totalPrice = (
       this.orderSum -
       this.orderSum * this.discount +
       this.orderSum * 0.02
     ).toFixed(2);
 
+    this.displayInfoOrderInBill();
+  }
+
+  displayInfoOrderInBill() {
+    this.totalPriceFieldInBill.value = this.orderSum; //order-sum without discont and show at Item Total(bill)
+    this.discountInBill.value = this.discount * 100;
     this.toPayFieldBill.value = this.totalPrice;
+  }
+
+  cleanOrderListBill() {
+    if (this.outputInfoBill.children.length !== 0) {
+      DOMHelper.removeListEls(this.outputInfoBill.children);
+    }
+  }
+
+  cleanAllBillInfo() {
+    this.cleanAllInBill.addEventListener("click", () => {
+      this.cleanOrderListBill();
+      this.totalPriceFieldInBill.value = "";
+      this.discountInBill.value = "";
+      this.toPayFieldBill.value = "";
+      this.setEmptyOrder();
+
+      // dataFromClient.info = "Pizza with: ";
+    });
+  }
+
+  setEmptyOrder() {
+    this.orders = [];
+    this.totalPrice = 0;
+    this.discount = 0;
+    this.orderSum = 0;
   }
 }
